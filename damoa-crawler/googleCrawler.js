2c1e22f9fcb212758895d7a3c7317266f1b237a2
Ahyun Kim <zlzzlzz2l@naver.com>
2021-10-02 10:27:17 +0900

    크롤링 자동화 추가

--- damoa-crawler/googleCrawler.js
+++ damoa-crawler/googleCrawler.js
@@ -1,33 +1,37 @@
+const cron = require("node-cron"); // 스케쥴러
 const axios = require("axios"); //특정  URL  삽입 시 URL html 태그 가지고
 const cheerio = require("cheerio");
 
-// HTML 코드를 가지고 오는 함수 (기사 최신순)
-const getHTML = async(keyword) => {
-  try{
-    return await axios.get("https://news.google.com/search?q=" + encodeURI(keyword) + "%20when%3A1d&hl=ko&gl=KR&ceid=KR%3Ako") //""안에는 URL 삽입
-  }catch(err) {
-    console.log(err);
+cron.schedule('*/1 * * * *', () => {
+  console.log('1분마다 실행: ', new Date().toString());
+  // HTML 코드를 가지고 오는 함수 (기사 최신순)
+  const getHTML = async(keyword) => {
+    try{
+      return await axios.get("https://news.google.com/search?q=" + encodeURI(keyword) + "%20when%3A1d&hl=ko&gl=KR&ceid=KR%3Ako") //""안에는 URL 삽입
+    }catch(err) {
+      console.log(err);
+    }
   }
-}
 
- // 파싱 함수
-const parsing = async (keyword) => {
-  const html = await getHTML(keyword);
-  const $ = cheerio.load(html.data);// 가지고 오는 data load
-  const $news = $("div.NiLAwe, div.y6IFtc, div.R7GTQ, div.keNKEd, div.j7vNaf, div.nID9nc");
+   // 파싱 함수
+  const parsing = async (keyword) => {
+    const html = await getHTML(keyword);
+    const $ = cheerio.load(html.data);// 가지고 오는 data load
+    const $news = $("div.NiLAwe, div.y6IFtc, div.R7GTQ, div.keNKEd, div.j7vNaf, div.nID9nc");
 
-  let informations = [];
-  $news.each((idx,node) => {
-    informations.push({
-      title: $(node).find("article > h3 > a").text(), // 제목
-      press: $(node).find(".SVJrMe > a").text(), // 언론
-      time: $(node).find(".SVJrMe > time").text(), // 시간
-      // contents: $(node).find(".dsc_wrap").text(), // 내용
-      link : $(node).find("article > .VDXfz").attr("href"), // 본문 링크
-      img : $(node).find("a > figure > img").attr("src") // 이미지
-    })
-  });
-  console.log(informations);
-}
+    let informations = [];
+    $news.each((idx,node) => {
+      informations.push({
+        title: $(node).find("article > h3 > a").text(), // 제목
+        press: $(node).find(".SVJrMe > a").text(), // 언론
+        time: $(node).find(".SVJrMe > time").text(), // 시간
+        // contents: $(node).find(".dsc_wrap").text(), // 내용
+        link : $(node).find("article > .VDXfz").attr("href"), // 본문 링크
+        img : $(node).find("a > figure > img").attr("src") // 이미지
+      })
+    });
+    console.log(informations);
+  }
 
-parsing("개발자 채용");
+  parsing("개발자 채용");
+})
--- damoa-crawler/naverCrawler.js
+++ damoa-crawler/naverCrawler.js
@@ -1,33 +1,37 @@
+const cron = require("node-cron"); // 스케쥴러
 const axios = require("axios"); //특정  URL  삽입 시 URL html 태그 가지고
 const cheerio = require("cheerio");
 
-// HTML 코드를 가지고 오는 함수
-const getHTML = async(keyword) => {
-  try{
-    return await axios.get("https://search.naver.com/search.naver?where=news&query=" + encodeURI(keyword) + "&sm=tab_opt&sort=1&photo=0&field=0&pd=0&ds=&de=&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Add%2Cp%3Aall&is_sug_officeid=0" ); //""안에는 URL 삽입
-  }catch(err) {
-    console.log(err);
+cron.schedule('0 9,12,18 * * *', () => {
+  console.log('9시, 12시, 6시에 실행: ', new Date().toString());
+  // HTML 코드를 가지고 오는 함수
+  const getHTML = async(keyword) => {
+    try{
+      return await axios.get("https://search.naver.com/search.naver?where=news&query=" + encodeURI(keyword) + "&sm=tab_opt&sort=1&photo=0&field=0&pd=0&ds=&de=&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Add%2Cp%3Aall&is_sug_officeid=0" ); //""안에는 URL 삽입
+    }catch(err) {
+      console.log(err);
+    }
   }
-}
 
- // 파싱 함수
-const parsing = async (keyword) => {
-  const html = await getHTML(keyword)
-  const $ = cheerio.load(html.data);// 가지고 오는 data load
-  const $news = $(".list_news > .bx");
+   // 파싱 함수
+  const parsing = async (keyword) => {
+    const html = await getHTML(keyword)
+    const $ = cheerio.load(html.data);// 가지고 오는 data load
+    const $news = $(".list_news > .bx");
 
-  let informations = [];
-  $news.each((idx,node) => {
-    informations.push({
-      title: $(node).find(".news_tit").text(), // 제목
-      press: $(node).find(".info_group > .press").text(), // 언론
-      time: $(node).find(".info_group > span").text(), // 시간
-      // contents: $(node).find(".dsc_wrap").text(), // 내용
-      link : $(node).find(".news_tit").attr("href"), // 본문 링크
-      img : $(node).find("a > img").attr("src") // 이미지
-    })
-  }); //for문과 동일
-  console.log(informations);
-}
+    let informations = [];
+    $news.each((idx,node) => {
+      informations.push({
+        title: $(node).find(".news_tit").text(), // 제목
+        press: $(node).find(".info_group > .press").text(), // 언론
+        time: $(node).find(".info_group > span").text(), // 시간
+        // contents: $(node).find(".dsc_wrap").text(), // 내용
+        link : $(node).find(".news_tit").attr("href"), // 본문 링크
+        img : $(node).find("a > img").attr("src") // 이미지
+      })
+    }); //for문과 동일
+    console.log(informations);
+  }
 
-parsing("개발자 채용");
+  parsing("개발자 채용");
+})
